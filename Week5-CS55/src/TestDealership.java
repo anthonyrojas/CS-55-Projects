ec55bccbb2c76867eb5c5d2a36001a0f82a23edc
anthonyrojas <anthony.rojas095@gmail.com>
2017-07-23 12:32:02 -0700

    Create TestDealership.java

---
+++ Week5-CS55/src/TestDealership.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.event.*;
+import java.util.*;
+import java.io.*;
+import java.text.*;
+
+public class TestDealership{
+
+	public static void main(String [] args){
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+		Manager m = new Manager("Rob", "Stark", "123 Winterfell Rd.", new GregorianCalendar(1999, 00, 14)); //year, month, day ...month starts at 0 and ends at 11
+		
+	}
+}
