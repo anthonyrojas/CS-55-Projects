e8444692d0d4cc7779645f25e8d87c3fd0eda7d0
anthonyrojas <anthony.rojas095@gmail.com>
2017-07-22 20:16:26 -0700

    Update Car.java

--- Week5-CS55/src/Car.java
+++ Week5-CS55/src/Car.java
@@ -14,4 +14,48 @@ public class Car {
 	public Car(){
 		
 	}
+
+	public Car(String m, double p, int y, int vN){
+		make = m;
+		price = p;
+		year = y;
+		vinNumber = vN;
+	}
+
+	public void setMake(String m){
+		this.make = m;
+	}
+
+	public void setPrice(double price){
+		this.price = p;
+	}
+
+	public void setYear(int y){
+		this.year = y;
+	}
+
+	public void setVinNumber(int vN){
+		this.vinNumber = vN;
+	}
+
+	public String getMake(){
+		return make;
+	}
+
+	public double getPrice(){
+		return price;
+	}
+
+	public int getYear(){
+		return year;
+	}
+
+	public int getVinNumber(){
+		return vinNumber;
+	}
+
+	@Override
+	public String toString(){
+		return make + "\t" + String.valueOf(price) + "\t" + String.valueOf(year) + "\t" + String.valueOf(vinNumber); 
+	}
 }
