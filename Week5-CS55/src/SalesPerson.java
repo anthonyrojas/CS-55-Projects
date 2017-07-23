0389b9026f9e14a37fc38e3fb84d634e8e9bad69
anthonyrojas <anthony.rojas095@gmail.com>
2017-07-22 20:17:33 -0700

    Update SalesPerson.java

--- Week5-CS55/src/SalesPerson.java
+++ Week5-CS55/src/SalesPerson.java
@@ -14,4 +14,61 @@ public class SalesPerson {
 	public SalesPerson(){
 		
 	}
+
+	public SalesPerson(String fn, String ln, double b, String a, Date hd){
+		firstName = fn;
+		lastName = ln;
+		bonus = b;
+		address = a;
+		hiringDate = hd;
+	}
+
+	public void setFirstName(String fn){
+		this.firstName = fn;
+	}
+
+	public void setLastName(String ln){
+		this.lastName = ln;
+	}
+
+	public void setBonus(double b){
+		this.bonus = b;
+	}
+
+	public void setAddress(String a){
+		this.address = a;
+	}
+
+	public void setHiringDate(Date hd){
+		this.hiringDate = hd;
+	}
+
+	public String getFirstName(){
+		return firstName;
+	}
+
+	public String getLastName(){
+		return lastName;
+	}
+
+	public double getBonus(){
+		return bonus;
+	}
+
+	public String getAddress(){
+		return address;
+	}
+
+	public Date getHiringDate(){
+		return hiringDate;
+	}
+
+	public String getCalendarDate(Date d){
+		return String.valueOf(d.getMonth()) + "/" + String.valueOf(d.getDay()) + "/" + String.valueOf(d.getYear());
+	}
+
+	@Override
+	public String toString(){
+		return firstName + " " + lastName + "\n" + address + "\n$" + bonus + "\n" + getCalendarDate(hiringDate);
+	}
 }
